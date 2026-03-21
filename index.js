b53decc728e60a45218bd6e433009559332d849a
anhtuank28 <nguyenanhtuank18@gmail.com>
2026-03-21 15:22:36 +0700

    nhung file tinh

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "pug");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   res.render("client/pages/home.pug");
 });
