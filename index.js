5c7e7433be87b96a9ec883b7c6fa90925c873dfa
Anh Tuấn <nguyenanhtuank18@gmail.com>
2026-03-21 12:07:16 +0700

    khoi tao prj 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Trang chủ");
+});
+app.get("/tours", (req, res) => {
+  res.send("Danh sách tour");
+});
+
+app.listen(port, () => {
+  console.log(`Web đang chạy trên cổng ${port}`);
+});
