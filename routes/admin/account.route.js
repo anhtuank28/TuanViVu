2c3d2ad72bc19fba50d40bbb14236cfbf5db2abf
anhtuank28 <nguyenanhtuank18@gmail.com>
2026-04-01 22:14:08 +0700

    validate pass

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 
 const accountController = require("../../controllers/admin/account.controller");
-
+const accountValidate = require("../../validates/admin/account.validate");
 router.get("/login", accountController.login);
 router.get("/register", accountController.register);
-router.post("/register", accountController.registerPost);
+router.post(
+  "/register",
+  accountValidate.registerPost,
+  accountController.registerPost,
+);
 router.get("/register-initial", accountController.registerInitial);
 router.get("/forgot-pasword", accountController.forgotPassword);
 router.get("/otp-pasword", accountController.otpPassword);
---
+++ validates/admin/account.validate.js
@@ -0,0 +1,55 @@
+const Joi = require("joi");
+
+module.exports.registerPost = (req, res, next) => {
+  const schema = Joi.object({
+    fullName: Joi.string()
+      .required()
+      .messages({
+      "string.empty": "Vui lòng nhập họ tên",
+    }),
+    email: Joi.string()
+      .required()
+      .email()
+      .messages({
+      "string.empty": "Vui lòng nhập email",
+      "string.email":"Email không đúng định dạng"
+    }),
+    password: Joi.string()
+      .required()
+      .min(8)
+      .custom((value,helpers)=>{
+        if(!/[A-Z]/.test(value)){
+          return helpers.error("password.uppercase");
+        }
+        if(!/[a-z]/.test(value)){
+          return helpers.error("password.lowercase");
+        }
+        
+        if(!/\d/.test(value)){
+          return helpers.error("password.number");
+        }
+        if(!/[@$!%*?&#]/.test(value)){
+          return helpers.error("password.special");
+        }
+        return value;
+      })
+      .messages({
+      "string.empty": "Vui lòng nhập mật khẩu",
+      "string.min": "Mật khẩu phải có ít nhất 8 ký tự",
+      "password.uppercase":"Mật khẩu phải chứa ít nhất 1 chữ in hoa",
+      "password.lowercase":"Mật khẩu phải chứa ít nhất 1 chữ cái thường",
+      "password.number":"Mật khẩu phải chứa ít nhất một chữ số!"
+    }),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    const errorMessage = error.details[0].message;
+    res.json({
+      code: "error",
+      message: errorMessage,
+    });
+    return;
+  }
+  next();
+};
