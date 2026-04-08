4e7edeb2d8094763970a15062990c8ae3c2c856f
anhtuank28 <nguyenanhtuank18@gmail.com>
2026-04-08 23:29:44 +0700

    sửa danh mục

--- routes/admin/category.route.js
+++ routes/admin/category.route.js
@@ -12,4 +12,6 @@ const upload=multer({storage: cloudinaryHelper.storage});
 router.get("/list", categoryController.list);
 router.get("/create", categoryController.create);
 router.post("/create",upload.single("avatar"),categoryValidate.createPost, categoryController.createPost);
+router.get("/edit/:id",categoryController.edit)
+router.patch("/edit/:id",upload.single("avatar"),categoryValidate.createPost,categoryController.editPatch)
 module.exports = router;
