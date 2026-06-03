4bd14d04fd2cf0940b06a98ead86e991bb9fd012
anhtuank28 <nguyenanhtuank18@gmail.com>
2026-06-03 21:49:47 +0700

    thong tin website

--- controllers/admin/setting.controller.js
+++ controllers/admin/setting.controller.js
@@ -1,11 +1,58 @@
+const SettingwebsiteInfo = require("../../models/setting-website-model")
+
+
 module.exports.list = async (req, res) => {
   res.render("admin/pages/setting-list", {
     pageTitle: "cài đặt chung",
   });
 };
 module.exports.websiteInfo = async (req, res) => {
+  let settingWebsiteInfo = await SettingwebsiteInfo.findOne({});
+
+  if (!settingWebsiteInfo) {
+    settingWebsiteInfo = {
+      websiteName: "",
+      phone: "",
+      email: "",
+      address: "",
+      logo: "",
+      favicon: ""
+    };
+  }
+
   res.render("admin/pages/setting-website-info", {
     pageTitle: "thông tin website",
+    settingWebsiteInfo: settingWebsiteInfo
+  });
+};
+module.exports.websiteInfoPatch = async (req, res) => {
+  if(req.files && req.files.logo){
+    req.body.logo = req.files.logo[0].path;
+  } else {
+    delete req.body.logo;
+  }
+  
+  if(req.files && req.files.favicon){
+    req.body.favicon = req.files.favicon[0].path;
+  } else {
+    delete req.body.favicon;
+  }
+
+  const settingwebsiteInfo = await SettingwebsiteInfo.findOne({});
+  if (settingwebsiteInfo) {
+    await SettingwebsiteInfo.updateOne({ _id: settingwebsiteInfo.id }, req.body);
+  } else {
+    const newRecord = new SettingwebsiteInfo(req.body);
+    await newRecord.save();
+  }
+
+  // Sửa res.flash thành req.flash (nếu bạn dùng thư viện express-flash)
+  if (req.flash) {
+    req.flash("success", "Cập nhật thành công");
+  }
+
+  res.json({
+    code: "success"
   });
 };
 module.exports.accountAdminList = async (req, res) => {
--- routes/admin/setting.route.js
+++ routes/admin/setting.route.js
@@ -1,12 +1,20 @@
 const router = require("express").Router();
+const multer = require("multer"); //thư viện giúp upload lên các file, video, ảnh
+const cloudinaryHelper = require("../../helpers/cloudinary.helper");
+
+const upload = multer({ storage: cloudinaryHelper.storage });
 
 const settingController = require("../../controllers/admin/setting.controller");
 
 router.get("/list", settingController.list);
-router.get("./website-info", settingController.websiteInfo);
-router.get("./account-admin/list", settingController.accountAdminList);
-router.get("./account-admin/create", settingController.accountAdminCreate);
-router.get("./role/list", settingController.roleList);
-router.get("./role/create", settingController.roleCreate);
+router.get("/website-info", settingController.websiteInfo);
+router.patch("/website-info",upload.fields([
+    {name:"logo", maxCount: 1},
+    {name: "favicon",maxCount:1}
+]), settingController.websiteInfoPatch);
+router.get("/account-admin/list", settingController.accountAdminList);
+router.get("/account-admin/create", settingController.accountAdminCreate);
+router.get("/role/list", settingController.roleList);
+router.get("/role/create", settingController.roleCreate);
 
 module.exports = router;
