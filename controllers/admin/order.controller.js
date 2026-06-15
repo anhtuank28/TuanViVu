ccd7ee04183bee148fd7b0b8729b3abd341b492a
anhtuank28 <nguyenanhtuank18@gmail.com>
2026-06-15 14:49:08 +0700

    tính năng sửa đơn hàng

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -1,4 +1,5 @@
 const Order = require("../../models/order.model");
+const City = require("../../models/city.model");
 const variableConfig=require("../../config/variable");
 const moment = require("moment");
 
@@ -63,7 +64,64 @@ module.exports.list = async (req, res) => {
   });
 };
 module.exports.edit = async (req, res) => {
+  try{
+  const id=req.params.id;
+
+  const orderDetail=await Order.findOne({
+    _id:id,
+    deleted:false
+  })
+  orderDetail.createdAtFormat=moment(orderDetail.createdAt).format("YYYY-MM-DDTHH:mm");
+  for(const item of orderDetail.items){
+    const city=await City.findOne({
+      _id:item.locationFrom
+    })
+    item.locationFromName=city.name;
+    item.departureDateFormat=moment(item.departureDate).format("DD/MM/YYYY");
+  }
+
   res.render("admin/pages/order-edit", {
-    pageTitle: "Đơn hàng",
+    pageTitle: `Đơn hàng ${orderDetail.orderCode}`,
+    orderDetail:orderDetail,
+    paymentMethod:variableConfig.paymentMethod,
+        paymentStatus:variableConfig.paymentStatus,
+        orderStatus:variableConfig.orderStatus
+
   });
+  }catch(error){
+    console.error(error);
+    res.redirect(`/${variableConfig.pathAdmin}/order/list`);
+  }
+
 };
+module.exports.editPatch=async(req,res)=>{
+  try{
+    const id=req.params.id;
+    const order=await Order.findOne({
+      _id:id,
+      deleted:false
+    });
+    if(!order){
+      res.json({
+        code:'error',
+        message:"Thông tin đơn hàng không hợp lệ"
+      })
+      return;
+    }
+
+    await Order.updateOne({
+      _id:id,
+      deleted:false
+    },req.body);
+    req.flash("success","Cập nhập thay đổi thành công");
+    res.json({
+      code:"success"
+    }
+    )
+  }catch(error)
+{
+  res.json({
+    code:"error",
+    message:"thông tin đơn hàng không hợp lệ"
+  })
+}}
--- routes/admin/order.route.js
+++ routes/admin/order.route.js
@@ -5,6 +5,6 @@ const orderController = require("../../controllers/admin/order.controller");
 const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
 router.get("/list", authMiddleware.requireAuth("order-view"), orderController.list);
-router.get("/edit", authMiddleware.requireAuth("order-edit"), orderController.edit);
-
+router.get("/edit/:id", authMiddleware.requireAuth("order-edit"), orderController.edit);
+router.patch("/edit/:id",authMiddleware.requireAuth("order-edit"), orderController.editPatch)
 module.exports = router;
